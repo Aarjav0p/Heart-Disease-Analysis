bfcdd2e751da95a8b15ef4ae6a2470e24a3e1be0
Aarjav0p <aarjavpatriya74@gmail.com>
2026-08-24 20:16:01 +0530

    page tsx fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-use client'
+'use client';
 
-import { useMemo, useState } from 'react'
+import { useMemo, useState } from 'react';
 import {
   Activity, BarChart3, BrainCircuit, ChevronDown, CircleHelp, Gauge,
   HeartPulse, LayoutDashboard, Menu, RotateCcw, Settings2, ShieldCheck,
